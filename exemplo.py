3e7ad524e7045c6ce683e7392520302a5010747d
Xavier Kooijman <117608912+xavierkooijman@users.noreply.github.com>
2022-12-15 17:28:07 +0000

    a

--- exemplo.py
+++ exemplo.py
@@ -1,7 +1,6 @@
 from tkinter import *
 #https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/create_oval.html
 
-
 def criar_board():
 
     i = IntVar()
@@ -12,22 +11,26 @@ def criar_board():
             canvas.create_oval(20,20,70,70, fill="", outline="white")
             canvas.grid(row=i,column=j)
 
+def jogada(coluna):
+        canvas = Canvas(window,width=85,height=85, bg="black")
+        canvas.create_oval(20,20,70,70, fill="red", outline="")
+        canvas.grid(row=5,column=coluna)
+
 window = Tk()
 window.geometry("1200x900")
 window.title("4 em linha")
 window.configure(bg="black")
 
+coluna = IntVar()
+
 criar_board()
 
-label_linha = Label(window, text="Linha: ")
-label_linha.place(x=40,y=600)
-entry_linha = Entry(window)
-entry_linha.place(x=100, y=600)
-label_coluna = Label(window, text="Coluna: ")
-label_coluna.place(x=40,y=650)
-entry_coluna = Entry(window)
-entry_coluna.place(x=100, y=650)
+label_coluna = Label(window, text="Coluna(1 a 7): ")
+label_coluna.place(x=40,y=600)
+entry_coluna = Entry(window, textvariable=coluna)
+entry_coluna.place(x=140, y=600)
 
-
+botao_jogar = Button(window, text="Jogar", width=31, height=5, command=lambda:jogada(coluna))
+botao_jogar.place(x=40,y=650)
 
 window.mainloop()
